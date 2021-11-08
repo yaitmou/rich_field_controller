3c82d355cac331a77ace668390cfeb68bc4039f2
Younss Ait Mou <younss@Younsss-MacBook-Pro-2.local>
2021-11-08 15:10:35 +0300

    bug fix

--- test/src/rich_paragraph_test.dart
+++ test/src/rich_paragraph_test.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:rich_field_controller/rich_field_controller.dart';
 
 void main() {
   group('RichElement', () {
-    late RichParagraph paragraph;
+    // late RichParagraph paragraph;
     setUp(() {
-      paragraph = RichParagraph(FocusNode());
+      // paragraph = RichParagraph(FocusNode());
     });
     test('Given,.. When .. Then..', () {
       // ARRANGE
