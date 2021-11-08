9dcbb21d865c1bba641db10b7b6ab752282c0385
Younss Ait Mou <younss@Younsss-MacBook-Pro-2.local>
2021-11-08 15:19:57 +0300

    updated test files

---
+++ test/src/rich_field_controller_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('RichFieldController', () {
+    // late RichParagraph paragraph;
+    setUp(() {
+      // paragraph = RichParagraph(FocusNode());
+    });
+    test('Given,.. When .. Then..', () {
+      // ARRANGE
+
+      // ACT
+
+      // ASSERT
+    });
+  });
+}
